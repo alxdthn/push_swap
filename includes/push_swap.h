b00ae628f1d5d0c35bcbd45eed194b7e40cb995e
Natalia Alexandria <nalexand@at-e2.21-school.ru>
2019-07-16 16:56:46 +0300

    includes norm

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nalexand <nalexand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 18:13:31 by nalexand          #+#    #+#             */
-/*   Updated: 2019/07/16 16:08:47 by nalexand         ###   ########.fr       */
+/*   Updated: 2019/07/16 16:56:19 by nalexand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,8 @@
 # include "libft.h"
 # include "ft_printf.h"
 # include <mlx.h>
-# define PS_USAGE		"usage: push_swap [-h] array\n"\
-						"array: non-repeating ints\n"\
-						"example: push_swap [-h] 2 5 1 3"
-# define CH_USAGE		"usage: checker [-v] array\n"\
-						"array: non-repeating ints\n"\
-						"example: checker [-h] 2 5 1 3"
+# define PS_USAGE		"usage: push_swap [-h] [array of non-repeat ints]"
+# define CH_USAGE		"usage: checker [-v] [array of non-repeat ints]w"
 # define PS_MEM_ERR		"push_swap: Mem alocate failture!"
 # define CH_MEM_ERR		"checker: Mem alocate failture!"
 # define PS_ARG_ERR		"push_swap: Invalid argument!"
@@ -34,7 +30,6 @@
 # define PS_INPUT_ERR	"push_swap: Uncknown input error"
 # define CH_INPUT_ERR	"checker: Uncknown input error"
 # define SALUT			"push_swap: Welcome to handle mode"
-
 # define LEFT_BACKGROUND 0x252526
 # define RIGHT_BACKGROUND 0x1E1E1E
 # define POS_LINE_COLOR 0xC3C39D
@@ -42,15 +37,12 @@
 # define TOP_COLOR 0xfdfdfd
 # define MID_COLOR 0x1c4fbb
 # define BOT_COLOR 0xb61b1b
-
-
 # define ESC 53
 # define ENTER 36
 # define RIGHT 124
 # define LEFT 123
 # define UP 126
 # define DOWN 125
-
 # define PUSH_SWAP 22
 # define CHECKER 21
 # define QUIT -1
@@ -151,39 +143,39 @@ typedef struct	s_all
 	char		is_print;
 }				t_all;
 
-void	init(t_all *all, int ac, char **av);
-void	push(int *src, int *dst, char cmd, char flag);
-void	swap(int *arr, char cmd, char flag);
-void	rotate(int *arr, char dir, char cmd, char flag);
-char	read_cmd(char *cmd,  size_t *ofset);
-char	read_push(t_task *task);
-char	read_swap(t_task *task);
-char	read_rotate(t_task *task);
-void	push_swap_clear_exit(t_all *all, char *message);
-void	print_arr(int *a, int *b);
-int		is_sorted(int *arr);
-int		is_loop_sorted(int *arr, int adr);
-void	check_matches(t_all *all);
-void	print_lst(t_list *lst);
-void	get_info(t_info *info, int *a);
-void	visualisation_init(t_all *all);
-void	render(t_all *all, char cmd);
-size_t	get_cmd(t_all *all, char **line);
-void	handle_mode(t_all *all);
-void	insert_method(t_all *all);
-void	hard_insert_method(t_all *all);
+void			init(t_all *all, int ac, char **av);
+void			push(int *src, int *dst, char cmd, char flag);
+void			swap(int *arr, char cmd, char flag);
+void			rotate(int *arr, char dir, char cmd, char flag);
+char			read_cmd(char *cmd, size_t *ofset);
+char			read_push(t_task *task);
+char			read_swap(t_task *task);
+char			read_rotate(t_task *task);
+void			push_swap_clear_exit(t_all *all, char *message);
+void			print_arr(int *a, int *b);
+int				is_sorted(int *arr);
+int				is_loop_sorted(int *arr, int adr);
+void			check_matches(t_all *all);
+void			print_lst(t_list *lst);
+void			get_info(t_info *info, int *a);
+void			visualisation_init(t_all *all);
+void			render(t_all *all, char cmd);
+size_t			get_cmd(t_all *all, char **line);
+void			handle_mode(t_all *all);
+void			insert_method(t_all *all);
+void			hard_insert_method(t_all *all);
 
-void	make_cmd(t_all *all, char cmd);
-void	solve_operations(t_all *all, t_oprs oprs);
-void	get_double_rotation(int *a, int *b, int *rr);
-void	init_opers(t_oprs *oprs);
-int		get_rotation(int *r, int *rr, int size, int adr);
+void			make_cmd(t_all *all, char cmd);
+void			solve_operations(t_all *all, t_oprs oprs);
+void			get_double_rotation(int *a, int *b, int *rr);
+void			init_opers(t_oprs *oprs);
+int				get_rotation(int *r, int *rr, int size, int adr);
 
-int		get_value_adr(int *arr, int value);
-void	find_marks(t_all *all);
+int				get_value_adr(int *arr, int value);
+void			find_marks(t_all *all);
 
-int		deal_key(int key, t_all *all);
-int		loop_hook(t_all *all);
-int		key_press(int key, t_all *all);
+int				deal_key(int key, t_all *all);
+int				loop_hook(t_all *all);
+int				key_press(int key, t_all *all);
 
 #endif
